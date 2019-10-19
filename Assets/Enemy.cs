3b9856f841f01b056d11b60bc0c64bafe02d63a5
bfazzani <33535363+bfazzani@users.noreply.github.com>
2019-10-19 17:49:15 -0500

    Enemy Animations

---
+++ Assets/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    Animator anim;
+
+    [SerializeField]
+    private float speed = 0.7f;
+
+    private Vector3 player;
+
+    private void Awake()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim.SetBool("Walk_Anim", true);
+        player = Camera.main.transform.position;
+        player.y = transform.position.y;
+
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.LookAt(player);
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            player,
+            Time.deltaTime * speed);
+    }
+}
